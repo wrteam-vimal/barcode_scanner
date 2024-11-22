5f8a50c23747b66764ed4adb905464878a448b3d
wrteam-vimal <wrteam.vimal@gmail.com>
2024-11-22 15:21:37 +0530

    22 Nov 2024

--- lib/main.dart
+++ lib/main.dart
@@ -32,30 +32,6 @@ class MyAppState extends State<MyApp> {
     });
   }
 
-  Future<void> scanQR() async {
-    String barcodeScanRes;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-          '#ff6666', 'Cancel', true, ScanMode.QR);
-      if (kDebugMode) {
-        print(barcodeScanRes);
-      }
-    } on PlatformException {
-      barcodeScanRes = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _scanBarcode = barcodeScanRes;
-    });
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> scanBarcodeNormal() async {
     String barcodeScanRes;
     // Platform messages may fail, so we use a try/catch PlatformException.
@@ -95,9 +71,6 @@ class MyAppState extends State<MyApp> {
                   ElevatedButton(
                       onPressed: () => scanBarcodeNormal(),
                       child: const Text('Start barcode scan')),
-                  ElevatedButton(
-                      onPressed: () => scanQR(),
-                      child: const Text('Start QR scan')),
                   ElevatedButton(
                       onPressed: () => startBarcodeScanStream(),
                       child: const Text('Start barcode scan stream')),
